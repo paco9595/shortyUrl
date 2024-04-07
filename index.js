9ad37fa11c5c37eb937edc143c7c3c4fdbf1e525
Francisco VIllarreal <paco_vip95@hotmail.com>
2024-04-07 16:48:41 -0600

    shorturl

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://pacovillarreal9555:elementos9@cluster0.xlgk0lh.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connect  to MongoDB successfully"))
+  .catch((e) => console.log(e));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shoturls = await ShortUrl.find();
+  console.log(shoturls);
+  res.render("index", { shortUrls: shoturls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(process.env.PORT || 5000);
